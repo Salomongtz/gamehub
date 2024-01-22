7e96f19ef9f25b516812620746b9a7443d6b9ad7
PerZZi <emiliano.estebandiaz@gmail.com>
2024-01-22 18:25:20 -0300

    entidad Games

--- src/main/java/com/example/gamehub/models/Games.java
+++ src/main/java/com/example/gamehub/models/Games.java
@@ -1,2 +1,102 @@
-package com.example.gamehub.models;public class Game {
+package com.example.gamehub.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+@Entity
+public class Games {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Long sales;
+    private double price;
+    private LocalDate date;
+    private float discount;
+
+    public Games() {
+    }
+
+    public Games(Long id, String title, String description, Long sales, double price, LocalDate date, float discount) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.sales = sales;
+        this.price = price;
+        this.date = date;
+        this.discount = discount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getSales() {
+        return sales;
+    }
+
+    public void setSales(Long sales) {
+        this.sales = sales;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Games{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", sales=" + sales +
+                ", price=" + price +
+                ", date=" + date +
+                ", discount=" + discount +
+                '}';
+    }
 }
