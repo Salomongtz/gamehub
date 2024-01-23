215706c331ca6a2ddf9bbad9045cc823f4d85a10
PerZZi <emiliano.estebandiaz@gmail.com>
2024-01-23 11:13:27 -0300

    repositorios,dto y controller de games y purchaseGame

---
+++ src/main/java/com/example/gamehub/repositories/GamesRepository.java
@@ -0,0 +1,10 @@
+package com.example.gamehub.repositories;
+
+import com.example.gamehub.models.Games;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GamesRepository extends JpaRepository<Games, Long> {
+    boolean existsById (Long id);
+}
